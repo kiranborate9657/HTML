e51ff7140c9162c36a68de8266c39030c4f65624
Kiran Borate <boratek844@gmail.com>
2026-03-21 15:49:32 +0530

    Create dom3.js

---
+++ dom3.js
@@ -0,0 +1,6 @@
+let button= document.getElementById("hide");
+
+button.addEventListener("click", function() {
+    let paragraph = document.querySelector(".firstpage p");
+    paragraph.style.display = "none";
+});
