2c766e3f7d180801e34efcb3a93c62e1a702408a
Kiran Borate <boratek844@gmail.com>
2026-04-13 23:56:20 +0530

    Add student object with properties and logs

---
+++ Object.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Kiran",
+    age: 20,
+    course: "CSE"
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
